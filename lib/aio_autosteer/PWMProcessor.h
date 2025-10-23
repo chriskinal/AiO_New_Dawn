dbdd54429c0c26927a1fee391e3c3e9c3fa63ec1
m-elias <eliaschief@gmail.com>
2025-10-23 17:28:00 -0500

    Update comments for SPEED_PULSE_PIN and SPEED_PULSE_LED_PIN to clarify pin assignment from HardwareManager

--- lib/aio_autosteer/PWMProcessor.h
+++ lib/aio_autosteer/PWMProcessor.h
@@ -49,8 +49,8 @@ public:
 
 private:
     // Pin assignment from pcb.h
-    uint8_t SPEED_PULSE_PIN;      // D33 - actual speed pulse output, but get pin from HardwareManager
-    uint8_t SPEED_PULSE_LED_PIN;  // D37 - output for LED at 1/10 speed pulse frequency, but get pin from HardwareManager
+    uint8_t SPEED_PULSE_PIN;      // get pin from HardwareManager during init()
+    uint8_t SPEED_PULSE_LED_PIN;  // get pin from HardwareManager during init()
     
     // PWM parameters
     float pulseFrequency;      // Hz
